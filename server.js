ff7902fcde54863fad288eaa8a3ea141f81ee968
Sean <slevy@mediasignage.com>
2014-12-12 10:50:56 -0800

    milestone 2

--- server.js
+++ server.js
@@ -1,57 +1,49 @@
 var express = require("express");
 var app = express();
 var path = require('path');
+var $ = require('jquery').create();
+exports.$ = $;
 var _ = require('underscore');
 
-var userMap = {};
+///////////////
+// config
+///////////////
+var user = 'd25@ms.com'
+var pass = '123123';
+var stationID = 217;
+var refreshTimer = 5000;
+
 var userpass64 = "";
 var nowServing = 100;
 var staticDir = path.resolve(__dirname);
+var domain = '';
 
 app.use('/remoteValues/', express.static(staticDir));
-
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.favicon('./public/assets/favicon.ico'));
 
 var port = process.env.PORT || 8080;
 
-
-function onUserDomain(myData) {
-    var userDomain = myData.domain;
-    userMap[i_user] = userDomain;
-    //alert(userDomain);
-    send();
+function getDomain() {
+    var userPass = user + "," + pass;
+    var param = (new Buffer(userPass).toString('base64'));
+    param = param.replace(/=/g, ".");
+    param = param.replace(/[+]/g, "_");
+    userpass64 = param.replace(/[/]/g, "-");
+    var url1 = 'https://galaxy.signage.me/WebService/getUserDomain.ashx?i_userpass=' + userpass64;
+    $.get(url1, function (data) {
+        var regexp = /(domain: ")(.*)(.signage)/ig;
+        domain = regexp.exec(data)[2];
+        // hack workaround until setval fixed
+        sendCommand("next", '10');
+    });
 }
 
-function sendCommand(i_domain, i_user, i_password, i_stationId, i_senderName, i_param) {
-    if (i_param == undefined) {
-        i_param = "";
-    }
-    var userDomain = userMap[i_user];
-    if (userDomain == null) {
-        var userPass = i_user + "," + i_password;
-        var param = $.base64.encode(userPass);
-        param = param.replace(/=/g, ".");
-        param = param.replace(/[+]/g, "_");
-        var userpass64 = param.replace(/[/]/g, "-");
-
-        var url1 = 'https://galaxy.signage.me/WebService/getUserDomain.ashx?i_userpass=' + userpass64 + '&callback=?';
-        $.getJSON(url1, onUserDomain);
-    }
-    else {
-        send();
-    }
-
-    function send() {
-        var url2 = 'http://' + userDomain + '/WebService/sendCommand.ashx?i_userpass=' + userpass64 + '&i_stationId=' + i_stationId + '&i_command=event' + '&i_param1=' + i_senderName + '&i_param2=' + i_param + '&callback=?';
-        //alert(url2);
-        $.getJSON(url2, onSendCommand);
-    }
-
-    function onSendCommand(myData) {
-        //alert("onSendCommand");
-    }
+function sendCommand(i_event, i_value) {
+    console.log('sending ' + i_value);
+    var url2 = 'http://' + domain + '.signage.me/WebService/sendCommand.ashx?i_userpass=' + userpass64 + '&i_stationId=' + stationID + '&i_command=event' + '&i_param1=' + i_event + '&i_param2=' + i_value + '&callback=?';
+    $.get(url2, function (data) {
+    });
 }
 
 var startServer = function (port, fn) {
@@ -70,6 +62,11 @@ var startServer = function (port, fn) {
         .listen(port)
 };
 
+getDomain();
+
+setInterval(function () {
+    sendCommand("setval", nowServing);
+}, refreshTimer);
 
 startServer(8080, function (err) {
     if (err) {
@@ -97,7 +94,7 @@ startServer(8080, function (err) {
             return false;
         }
         res.send('{"nowServing": "' + ++nowServing + '"}');
-        sendCommand("galaxy.signage.me", "d25@ms.com", "xxx", "74", "setval", nowServing);
+        sendCommand("setval", nowServing);
     });
 
     app.all('/prev', function (req, res) {
@@ -110,7 +107,7 @@ startServer(8080, function (err) {
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header("Access-Control-Allow-Origin", "*");
         res.send('{"nowServing": "' + --nowServing + '"}');
-        sendCommand("galaxy.signage.me", "d25@ms.com", "xxx", "74", "setval", nowServing);
+        sendCommand("setval", nowServing);
     });
 
 
